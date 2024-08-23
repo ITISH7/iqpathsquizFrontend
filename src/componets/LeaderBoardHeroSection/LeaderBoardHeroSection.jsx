b518b50c2a4beb4121415315c2fe6058fb3ae793
Hricha Sharma <sharmahricha6@gmail.com>
2024-08-23 08:40:12 +0530

    add image in hero section

--- src/componets/LeaderBoardHeroSection/LeaderBoardHeroSection.jsx
+++ src/componets/LeaderBoardHeroSection/LeaderBoardHeroSection.jsx
@@ -4,8 +4,9 @@ import styles from './LeaderBoardHeroSection.module.css';
 const LeaderBoardHeroSection = () => {
   return (
     <div className={styles.container}>
+      <img src="src\assets\LeaderBoardHeroSection.svg" />
       <button className={styles.filterButton}>Filters</button>
-      <div className={styles.title}>DSA Challenge Leaderboard</div>
+      <div className={styles.title}></div> 
     </div>
   );
 };
