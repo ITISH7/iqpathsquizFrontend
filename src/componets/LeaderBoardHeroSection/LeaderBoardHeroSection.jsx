1ed42a7c7e768b911198f916174a6f5994d111a5
Vishesh Verma <visheshverma250703@gmail.com>
2024-08-23 09:12:14 +0530

    bug fixes

--- src/componets/LeaderBoardHeroSection/LeaderBoardHeroSection.jsx
+++ src/componets/LeaderBoardHeroSection/LeaderBoardHeroSection.jsx
@@ -4,9 +4,11 @@ import styles from './LeaderBoardHeroSection.module.css';
 const LeaderBoardHeroSection = () => {
   return (
     <div className={styles.container}>
-      <img src="src\assets\LeaderBoardHeroSection.svg" />
+      {/* <img src="src\assets\HeroSectionBg.png" /> */}
       <button className={styles.filterButton}>Filters</button>
-      <div className={styles.title}></div> 
+      <div className={styles.title}> Where you Stand?</div> 
+      <img src="src\assets\flyingplane.png" alt="" className={styles.image} />
+      <img src="src\assets\moutainClimbing.svg" alt="" className={styles.imageCorner} />
     </div>
   );
 };
