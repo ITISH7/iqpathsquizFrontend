bddd1580133f89f707644bac3657da47c66cbb8c
Vishesh Verma <visheshverma250703@gmail.com>
2024-08-24 21:44:06 +0530

    basic design complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from '../componets/HeroSection/HeroSection'
 import AddBlock from '../componets/AddBlock/AddBlock'
 import QuestionTopicDropDown from '../componets/QuestionTopicDropDown/QuestionTopicDropDown'
+import Statistics from '../componets/Statistics/Statistics'
 
 function Home() {
   return (
     <>
      <HeroSection />
      <AddBlock />
+     <Statistics />
      <QuestionTopicDropDown />
      <QuestionTopicDropDown />
      <QuestionTopicDropDown />
