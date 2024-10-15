f43b6cc2356c1c2462645243093842d449283751
Vishesh Verma <visheshverma250703@gmail.com>
2024-10-15 19:09:29 +0530

    added google login button

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../context/AuthContext";
 import * as Components from './login.js';
 import {  toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import styles from './login.module.css';
 
 const Log = ({ onSwitchToSignup }) => {
   const [errorMessage, setErrorMessage] = useState("");
@@ -143,104 +144,121 @@ const Log = ({ onSwitchToSignup }) => {
 
   return (
     <Components.Container>
-    <Components.SignUpContainer signinIn={signIn}>
-      <Components.Form onSubmit={handleSignup}>
-        <Components.Title>Create Account</Components.Title>
-  
-        {/* {errorMessage && (
+      <Components.SignUpContainer signinIn={signIn}>
+        <Components.Form onSubmit={handleSignup}>
+          <Components.Title>Create Account</Components.Title>
+
+          {/* {errorMessage && (
           <Components.ErrorMessage>{errorMessage}</Components.ErrorMessage>
         )}
    */}
-        <Components.Input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-          required
-        />
-        <Components.Input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          required
-        />
-        <Components.Input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          required
-        />
-        <Components.Input
-          type="tel"
-          placeholder="Mobile Number"
-          name="mobileNo"
-          value={formData.mobileNo}
-          onChange={handleInputChange}
-          required
-        />
-  
-        <Components.Button type="submit">Sign Up</Components.Button>
-      </Components.Form>
-    </Components.SignUpContainer>
-  
-    <Components.SignInContainer signinIn={signIn}>
-      <Components.Form onSubmit={handleLogin}>
-        <Components.Title>Sign in</Components.Title>
-  
-        {errorMessage && (
-          <Components.ErrorMessage>{errorMessage}</Components.ErrorMessage>
-        )}
-  
-        <Components.Input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          required
-        />
-        <Components.Input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          required
-        />
-        <Components.Anchor href="#">Forgot your password?</Components.Anchor>
-        <Components.Button type="submit">Sign In</Components.Button>
-      </Components.Form>
-    </Components.SignInContainer>
-  
-    <Components.OverlayContainer signinIn={signIn}>
-      <Components.Overlay signinIn={signIn}>
-        <Components.LeftOverlayPanel signinIn={signIn}>
-          <Components.Title>Welcome Back!</Components.Title>
-          <Components.Paragraph>
-            To keep connected with us, please login with your personal info
-          </Components.Paragraph>
-          <Components.GhostButton onClick={() => toggle(true)}>
-            Sign In
-          </Components.GhostButton>
-        </Components.LeftOverlayPanel>
-  
-        <Components.RightOverlayPanel signinIn={signIn}>
-          <Components.Title>Hello, Friend!</Components.Title>
-          <Components.Paragraph>
-            Enter your personal details and start your journey with us
-          </Components.Paragraph>
-          <Components.GhostButton onClick={() => toggle(false)}>
-            Sign Up
-          </Components.GhostButton>
-        </Components.RightOverlayPanel>
-      </Components.Overlay>
-    </Components.OverlayContainer>
-  </Components.Container>
+          <Components.Input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          <Components.Input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <Components.Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <Components.Input
+            type="tel"
+            placeholder="Mobile Number"
+            name="mobileNo"
+            value={formData.mobileNo}
+            onChange={handleInputChange}
+            required
+          />
+
+          <Components.Button type="submit">Sign Up</Components.Button>
+        </Components.Form>
+      </Components.SignUpContainer>
+
+      <Components.SignInContainer signinIn={signIn}>
+        <Components.Form onSubmit={handleLogin}>
+          <Components.Title>Sign in</Components.Title>
+
+          {errorMessage && (
+            <Components.ErrorMessage>{errorMessage}</Components.ErrorMessage>
+          )}
+
+          <Components.Input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <Components.Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <Components.Anchor href="#">Forgot your password?</Components.Anchor>
+          <Components.Button type="submit">Sign In </Components.Button>
+          <button className={styles.googleButton}>
+            <img
+              src="src/assets/googleLogo.png"
+              alt="Google Logo"
+              className={styles.googleLogo}
+            />
+            Sign in with Google
+          </button>
+        </Components.Form>
+      </Components.SignInContainer>
+
+      <Components.OverlayContainer signinIn={signIn}>
+        <Components.Overlay signinIn={signIn}>
+          <Components.LeftOverlayPanel signinIn={signIn}>
+            <Components.Title>Welcome Back!</Components.Title>
+            <Components.Paragraph>
+              To keep connected with us, please login with your personal info
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(true)}>
+              Sign In
+            </Components.GhostButton>
+            <button className={styles.googleButton}>
+            <img
+              src="src/assets/googleLogo.png"
+              alt="Google Logo"
+              className={styles.googleLogo}
+            />
+            Sign up with Google
+          </button>
+          </Components.LeftOverlayPanel>
+
+          <Components.RightOverlayPanel signinIn={signIn}>
+            <Components.Title>Hello, Friend!</Components.Title>
+            <Components.Paragraph>
+              Enter your personal details and start your journey with us
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(false)}>
+              Sign Up
+            </Components.GhostButton>
+            
+          </Components.RightOverlayPanel>
+        </Components.Overlay>
+      </Components.OverlayContainer>
+    </Components.Container>
   );
 };
 
